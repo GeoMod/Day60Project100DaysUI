d3ce288c3d96e9693a7753f94f71e6d11be04279
Dan O'Leary <olearydaniel@me.com>
2019-11-22 11:05:24 -0600

    Initial Commit

---
+++ Day60 JSON Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day60 JSON Friends
+//
+//  Created by Daniel O'Leary on 11/22/19.
+//  Copyright © 2019 Impulse Coupled Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
